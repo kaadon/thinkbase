20d5acabc1bc47a46bbcc8d14a1967a2085f4f89
kaadon.com <kaadon.com@gmail.com>
2023-07-31 10:31:49 +0800

    first commit

--- src/helper.php
+++ src/helper.php
@@ -173,21 +173,41 @@ if (!function_exists('is_dev')) {
         return Env::get("app_dev") ?? false;
     }
 }
-
-if (!function_exists("get_conf")) {
-    /**
-     * 获取配置
-     * @param string $group
-     * @param string $name
-     * @param string|null $key
-     * @param array|string|int|float|bool $default
-     * @return mixed|string
-     */
-    function get_conf(string $group, string $name, ?string $key = null, array|string|int|float|bool $default = "")
-    {
-        return \Yaconf::get("{$group}.{$name}.{$key}") ?? (Env::get("{$group}.{$name}.{$key}") ?: $default);
-    }
-}
+	
+	if (!function_exists("get_conf")) {
+		/**
+		 * 获取配置
+		 *
+		 * @param string                      $group
+		 * @param string|null                 $name
+		 * @param array|string|int|float|bool $default
+		 * @param string                      $filePath
+		 *
+		 * @return array|string|int|float|bool
+		 */
+		function get_conf(string $group, ?string $name = null, array|string|int|float|bool $default = "",string $filePath = "app",): array|string|int|float|bool
+		{
+			$envPath = "{$group}";
+			if (!is_null($name)) $envPath .= $name;
+			return get_yaconf_config($group,$name,$filePath) ?? (Env::get($envPath) ?: $default);
+		}
+	}
+	
+	if (!function_exists('get_yaconf_config')) {
+		/**
+		 * @param string      $group
+		 * @param string|null $name
+		 * @param string      $fileName
+		 *
+		 * @return array|string|int|float|bool
+		 */
+		function get_yaconf_config(string $group, ?string $name = null, string $fileName = "app"):array|string|int|float|bool
+		{
+			$path = "{$fileName}.{$group}";
+			if (!is_null($name)) $path .= ".{$name}";
+			return \Yaconf::get($path);
+		}
+	}
 
 /** RETURN **/
 if (!function_exists('success_zip')) {
